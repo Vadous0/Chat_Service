1c5c6b58164ced2c2faf80737f50ec249238f736
JanWiedemann <jw706377@fh-muenster.de>
2023-05-15 01:27:40 +0200

    Init

--- src/main/java/ChatService/ChatClient/ChatClient.java
+++ src/main/java/ChatService/ChatClient/ChatClient.java
@@ -26,6 +26,7 @@ public class ChatClient extends Application {
     }
     // TODO Methode implementieren
     public void close_connection(){
+        System.out.println("Hello");
     }
     // TODO Methode implementieren
     public void handle_connection(){
